128247a9be6f46847f14f0c59b4dec30df56d581
Anastas Mironov <ast@l80.ru>
2022-01-16 23:24:00 +0900

    feat: add upload video method with thumbnail generate

--- src/Http/Controllers/FileController.php
+++ src/Http/Controllers/FileController.php
@@ -5,38 +5,36 @@ namespace Ast21\FileUploader\Http\Controllers;
 use Ast21\FileUploader\Http\Requests\FileUploadRequest;
 use Ast21\FileUploader\Http\Requests\VideoUploadRequest;
 use Ast21\FileUploader\Http\Services\FileService;
-use Ast21\FileUploader\Models\File;
 
 class FileController extends Controller
 {
-    protected $file;
     protected $fileService;
 
-    public function __construct(File $file, FileService $fileService)
+    public function __construct(FileService $fileService)
     {
-        $this->file = $file;
         $this->fileService = $fileService;
     }
 
     public function fileStore(FileUploadRequest $request)
     {
-        $file = $this->fileService->upload($request->file('file'));
+        $fileModel = $this->fileService->upload($request->file('file'));
 
         return response()->json([
-            'id' => $file->id,
-            'name' => $file->name,
-            'url' => asset($file->url),
+            'id' => $fileModel->id,
+            'name' => $fileModel->name,
+            'url' => asset($fileModel->url),
         ]);
     }
 
     public function videoStore(VideoUploadRequest $request)
     {
-        $file = $this->fileService->upload($request->file('file'));
+        $fileModel = $this->fileService->uploadVideo($request->file('file'));
 
         return response()->json([
-            'id' => $file->id,
-            'name' => $file->name,
-            'url' => asset($file->url),
+            'id' => $fileModel->id,
+            'thumbnail_id' => $fileModel->thumbnail_id,
+            'name' => $fileModel->name,
+            'url' => asset($fileModel->url),
         ]);
     }
 }
--- src/Http/Services/FileService.php
+++ src/Http/Services/FileService.php
@@ -4,15 +4,19 @@ namespace Ast21\FileUploader\Http\Services;
 
 use Ast21\FileUploader\Models\File;
 use Illuminate\Http\UploadedFile;
+use FFMpeg\FFMpeg;
+use FFMpeg\Coordinate\TimeCode;
 
 class FileService
 {
-    protected $file;
+    protected $fileModel;
     protected $fileObject;
 
-    public function __construct(File $file, FileObject $fileObject)
+    const VIDEO_THUMBNAIL_FRAME_SEC = 10;
+
+    public function __construct(File $fileModel, FileObject $fileObject)
     {
-        $this->file = $file;
+        $this->fileModel = $fileModel;
         $this->fileObject = $fileObject;
     }
 
@@ -20,7 +24,8 @@ class FileService
     {
         $fileObject = $this->fileObject->createFromUploadedFile($file);
 
-        return $this->file->create([
+        return $this->fileModel->create([
+            'thumbnail_id' => null,
             'name' => $fileObject->getName(),
             'disk' => $fileObject->getDisk(),
             'path' => $fileObject->getPath(),
@@ -28,6 +33,51 @@ class FileService
             'type' => $fileObject->getType(),
             'mime_type' => $fileObject->getMimeType(),
             'extension' => $fileObject->getExtension(),
+        ]);
+    }
+
+    public function uploadVideo(UploadedFile $file)
+    {
+        $this->fileObject->createFromUploadedFile($file);
+
+        // 1. создать thumbnail из video через ffmpeg
+        while (true) {
+            $tempThumbnailPath = '/tmp/' . uniqid('thumb_', true) . '.png';
+            if (!file_exists(sys_get_temp_dir() . $tempThumbnailPath)) break;
+        }
+
+        $video = FFMpeg::create()->open($file->getRealPath());
+        $frame = $video->frame(TimeCode::fromSeconds(self::VIDEO_THUMBNAIL_FRAME_SEC));
+        $frame->save($tempThumbnailPath);
+
+        // 2. создать fileObject для thumbnail
+        $thumbnailObject = new FileObject();
+        $thumbnailObject->createFromUploadedFile(new UploadedFile($tempThumbnailPath, 'video_thumbnail.png', 'image/png'));
+
+        unlink($tempThumbnailPath);
+
+        // 3. сохранить модель thumbnail
+        $thumbnailModel = $this->fileModel->create([
+            'thumbnail_id' => null,
+            'name' => $thumbnailObject->getName(),
+            'disk' => $thumbnailObject->getDisk(),
+            'path' => $thumbnailObject->getPath(),
+            'url' => $thumbnailObject->getUrl(),
+            'type' => $thumbnailObject->getType(),
+            'mime_type' => $thumbnailObject->getMimeType(),
+            'extension' => $thumbnailObject->getExtension(),
+        ]);
+
+        // 4. сохранить модель video
+        return $this->fileModel->create([
+            'thumbnail_id' => $thumbnailModel->id,
+            'name' => $this->fileObject->getName(),
+            'disk' => $this->fileObject->getDisk(),
+            'path' => $this->fileObject->getPath(),
+            'url' => $this->fileObject->getUrl(),
+            'type' => $this->fileObject->getType(),
+            'mime_type' => $this->fileObject->getMimeType(),
+            'extension' => $this->fileObject->getExtension(),
         ]);
     }
 }
